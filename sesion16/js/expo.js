b8c0ccb69107fcc3c2be7588bed5cdacef6b0c45
Crystian Culqui <Crystian.2001.culqui@gmail.com>
2021-10-25 12:45:38 -0500

    ...

---
+++ sesion16/js/expo.js
@@ -0,0 +1,8 @@
+function eval(String){
+    var miTexto = "3 + 5"
+eval(document.write("<h2> miTexto " + eval + "</h2>"))
+}
+function parseInt(cadena, base){
+    var midato = "34, 8";
+document.write (parseInt("21",8))
+}
